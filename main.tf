044f2ab32dbf1d353c61ed0508cdab5f5669184e
Justin Campbell <justin@justincampbell.me>
2015-12-22 16:10:29 -0500

    Taint resource

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ resource "template_file" "timestamp" {
   filename = "timestamp.txt"
 
   provisioner "local-exec" {
-    command = "date +%s.%N > timestamp.txt"
+    command = "date +%s > timestamp.txt"
+  }
+
+  provisioner "local-exec" {
+    command = "terraform taint template_file.timestamp"
   }
 }
 
