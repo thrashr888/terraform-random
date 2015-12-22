ebd4218d64d6b0a509f03d439cc0bd2a21358b83
Justin Campbell <justin@justincampbell.me>
2015-12-22 17:09:09 -0500

    Update timestamp var in Atlas

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
+variable "timestamp" {
+  default = "0"
+}
+
 resource "template_file" "timestamp" {
   filename = "timestamp.txt"
+
+  vars {
+    last_timestamp = "${var.timestamp}"
+  }
 
   provisioner "local-exec" {
     command = "date +%s > timestamp.txt"
   }
+
+  provisioner "local-exec" {
+    command = "curl -X PUT https://atlas.hashicorp.com/api/v1/environments/$ATLAS_CONFIGURATION_SLUG/variables --header X-Atlas-Token:$ATLAS_TOKEN --data variables[timestamp]=$(date +%s.%N)"
+  }
+}
+
+output "last-timestamp" {
+  value = "${var.timestamp}"
 }
 
 output "timestamp" {
