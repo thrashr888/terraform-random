b0433eabf22101fae8800eb6ad01ffdf9e592bf3
Justin Campbell <justin@justincampbell.me>
2016-11-02 10:58:50 -0400

    Use random_id resource instead of template_file

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
-data "template_file" "random" {
-  template = "${value}"
-
-  vars {
-    value = "${uuid()}"
+resource "random_id" "random" {
+  keepers {
+    uuid = "${uuid()}"
   }
-}
 
-resource "null_resource" "default" {
-  triggers {
-    template = "${data.template_file.random.rendered}"
-  }
+  byte_length = 8
 }
 
 output "random" {
-  value = "${data.template_file.random.rendered}"
+  value = "${random_id.random.hex}"
 }
